10a2bde4c51eac7ccd2ec6498d1bfbeadc54075c
Calin Florin TUDOR <calinf.tudor@gmail.com>
2023-05-28 17:17:09 +0300

    commit validations

--- backend/index.ts
+++ backend/index.ts
@@ -7,6 +7,10 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || '1234';
 const authRoutes = require('./routes/auth-routes');
+
+const validation = require('./utilValidation/joiValidation');
+const { registerValidation } = require('./schemaValidation/registerValidation');
+const { loginValidation } = require('./schemaValidation/loginValidation');
 export const mongoClass = new MongoDb();
 
 app.use(express.json());
@@ -15,6 +19,16 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 app.use('/api/auth', authRoutes);
+
+app.post("/", validation(registerValidation), (req,res)=> {
+  const {username, email, password, confirmPassword} = req.body;
+  res.send(`username: ${username}, email: ${email}, password: ${password}, confirmPassword: ${confirmPassword}`);
+})
+
+app.post("/", validation(loginValidation), (req,res)=> {
+  const {username, email, password} = req.body;
+  res.send(`username: ${username}, email: ${email}, password: ${password}`);
+})
 
 mongoClass.connect();
 mongoClass.db.on('error', console.error.bind(console, 'connection error:'));
