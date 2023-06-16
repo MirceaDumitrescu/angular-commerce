aee0e398e6bdb0ef0db63b82aac0755f42cebee0
Hionel <104563991+Hionel@users.noreply.github.com>
2023-06-16 16:45:55 +0300

    my-account routing and child components integrated

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,6 +13,11 @@ import { MyAccountComponent } from './pages/my-account/my-account.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
 import { ProductComponent } from './pages/product/product.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { DashboardComponent } from './pages/my-account/dashboard/dashboard.component';
+import { OrdersComponent } from './pages/my-account/orders/orders.component';
+import { PaymentMethodsComponent } from './pages/my-account/payment-methods/payment-methods.component';
+import { FavouritesComponent } from './pages/my-account/favourites/favourites.component';
+import { AddressesComponent } from './pages/my-account/addresses/addresses.component';
 
 const pages = [
   AppComponent,
@@ -30,7 +35,7 @@ const components = [
 ];
 
 @NgModule({
-  declarations: [...pages, ...components],
+  declarations: [...pages, ...components, DashboardComponent, OrdersComponent, PaymentMethodsComponent, FavouritesComponent, AddressesComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
