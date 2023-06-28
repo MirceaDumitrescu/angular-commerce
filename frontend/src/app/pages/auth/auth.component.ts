ec9b05f845f3ff66b1b832fa4a65fbee04eb8d9e
Calin Florin TUDOR <calinf.tudor@gmail.com>
2023-06-28 21:29:37 +0300

    snackbar

--- frontend/src/app/pages/auth/auth.component.ts
+++ frontend/src/app/pages/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -8,9 +9,17 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+email: any;
+password: any;
+username: any;
+firstname: any;
+lastname: any;
+confirmPassword: any;
 
   
-  constructor (private AuthService: AuthService) { }
+  constructor ( private AuthService: AuthService,
+                private _snackBar: MatSnackBar
+    ) { }
 
   registerForm = new FormGroup ({
 
@@ -30,15 +39,55 @@ export class AuthComponent {
     password: new FormControl("", [Validators.required, Validators.minLength(6)]),
   })
 
+
   submitRegistration = () => {
     console.log(this.registerForm.value)
 
     this.AuthService.register(this.registerForm.value)
+
   }
+
+  openRegisterSnackBar(username: any, email: any, firstname: any, lastname: any, password: any, confirmPassword: any) {
+    if(username && email && firstname && lastname && password && confirmPassword) {
+      this._snackBar.open('Sign Up successfully!', 'OK', {
+        duration: 2000,
+        verticalPosition : 'top',
+      })
+  } else if (password !== confirmPassword){
+    this._snackBar.open('Sign Up failed! Passwords do not match!', 'OK', {
+      duration: 2000,
+      verticalPosition : 'top',
+    })
+  } else {
+    this._snackBar.open('Sign Up failed!', 'OK', {
+      duration: 2000,
+      verticalPosition : 'top',
+    })
+  }
+}
 
   submitLogin = () => {
     console.log(this.loginForm.value)
 
     this.AuthService.login(this.loginForm.value)
+    
   }
+  
+  openLoginSnackBar(email: any, password: any) {
+    if(email && password) {
+
+      
+      this._snackBar.open('Sign In successfully!', 'OK', {
+        duration: 2000,
+        horizontalPosition : 'center',
+      })
+  } else {
+
+    
+    this._snackBar.open('Sign In failed!', 'OK', {
+      duration: 2000,
+      horizontalPosition : 'center',
+    })
+  }
+}
 }
